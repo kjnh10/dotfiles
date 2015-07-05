9901d1dc485a23a8fce9238f427f380495c37abd
Koji Watanabe@office <koji07082002@mail.benesse.co.jp>
2015-07-05 05:10:42 +0100

    modify install.sh to be able to deal with windows link

--- install.sh
+++ install.sh
@@ -1,9 +1,61 @@
-#!/bin/bash
+#!/bin/sh -e
 
-for f in .??*
+SCRIPT_DIR=$(dirname $0)
+
+## スクリプトが dotfiles ディレクトリ直下にあると想定
+DOTFILES_DIR=$SCRIPT_DIR
+
+function usage() {
+    echo "Usage: $0" 1>&2
+    exit 1
+}
+function error() {
+    echo "Error: $0 $1" 1>&2
+    exit 1
+}
+
+## 実行はホームディレクトリでなければならない
+if [ "$HOME" != "$(pwd)" ]; then
+    error "exec $0 at your home directory: $HOME";
+fi
+
+NOW=$(date +%Y%m%d%H%M%S)
+
+function mklink() {
+    NAME=$1
+    ## 既に同名のファイルが存在する場合はリネーム
+    if [ -e "$NAME" ]; then
+        mv $NAME ${NAME}.${NOW}
+    fi
+    DOTFILE=${DOTFILES_DIR}/${NAME}
+    echo "mklink $DOTFILE"
+    ## Windows の場合は cmd の mklink コマンドを使う
+    if [ -n "$WINDIR" ]; then
+        if [ -d "$DOTFILE" ]; then
+            ## 対象がディレクトリの場合にはジャンクション
+            MKLINK_OPTS="/J"
+        else
+            ## 対象がファイルの場合にはハードリンク
+            MKLINK_OPTS="/H"
+        fi
+        cmd <<< "mklink $MKLINK_OPTS \"${NAME%/}\" \"${DOTFILE%/}\"" > /dev/null
+    else
+        ln -s $DOTFILE
+    fi
+}
+
+## .git* 以外の dotfiles をリンク
+for F in $(find $DOTFILES_DIR -name '.*' -maxdepth 1 | grep -v '\.git');
 do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
+    NAME=$(basename $F)
+    mklink $NAME
+done
 
-    ln -sfnv "$f" ~/"$f"
+## 必要な .git* をリンク
+for NAME in .gitconfig;
+do
+    DOTFILE=${DOTFILES_DIR}/${NAME}
+    if [ -e "$DOTFILE" ]; then
+        mklink $NAME
+    fi
 done
